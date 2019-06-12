e36d40e1e212a253fe146e34957535abbf5d490e
J. Mario Rguez <juan.rodriguezp@atos.net>
2019-06-12 11:43:38 +0200

    Remove unused import

--- src/main/java/net/atos/ari/auth/controller/AuthController.java
+++ src/main/java/net/atos/ari/auth/controller/AuthController.java
@@ -30,7 +30,6 @@ import org.keycloak.representations.AccessTokenResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
