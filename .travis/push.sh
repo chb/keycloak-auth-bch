5d9863045f2bb92df8538f128a112b54c76ab49a
Carlos Cavero <carlos.cavero@atos.net>
2019-07-26 01:51:32 +0200

    Exclude .travis folder during the prepare stage and git add only the CHANGELOG and the pom files

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Commit the CHANGELOG modifications and new versioning
-git commit -am 'Automatic release increment and changelog generation. Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci]'
+git add CHANGELOG.md pom.xml
+git commit -m 'Automatic release increment and changelog generation [skip ci]'
 
 # Push the modifications into develop
 git push origin HEAD:master
